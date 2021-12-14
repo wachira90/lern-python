b0811e3fa32d6046460d1829241f896526ccfd17
Wachira Duangdee <wcrcorp@gmail.com>
2021-12-14 13:04:52 +0700

    Create TkinterScrollingWithText.py

---
+++ tkinter/TkinterScrollingWithText.py
@@ -0,0 +1,30 @@
+#!python
+from tkinter import *
+from tkinter.scrolledtext import *
+'''
+Make text scroll to the bottom of the text box as new entries appear
+###Tkinter Scrolling with Text###
+'''
+
+window = Tk()
+
+window.wm_title("Scroll From Bottom")
+
+TextBox = ScrolledText(window, height='10', width='45', wrap=WORD)
+
+#
+#Just adds 100 lines to the TextBox
+count = 0
+while(count<100):
+
+    TextBox.insert(END, "The count is: " +str(count)+"\n")
+
+    #Pushes the scrollbar and focus of text to the end of the text input.
+    TextBox.yview(END)
+    count += 1
+
+TextBox.pack()
+
+
+window = mainloop()
+
