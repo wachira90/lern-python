c92ac3d0978c5244d54a9fdcbe79275c7eab957c
Wachira Duangdee <wcrcorp@gmail.com>
2021-12-21 11:57:57 +0700

    Create read_with.py

---
+++ FileHandling/read_with.py
@@ -0,0 +1,15 @@
+#!python
+# -*- coding: utf-8 -*-
+import os
+
+# Reading files using 'with'
+with open('read_demo.txt', 'r') as file:
+    print(file.read())
+
+'''
+First line
+Second line
+Third line
+Fourth line
+Fifth line
+'''    
