e66e58875fe32b3f49f0529321cf70ae6f10cf49
Wachira Duangdee <wcrcorp@gmail.com>
2021-12-21 11:59:05 +0700

    Create read_line.py

---
+++ FileHandling/read_line.py
@@ -0,0 +1,13 @@
+#!python
+# -*- coding: utf-8 -*-
+import os
+
+with open('read_demo.txt', 'r') as fp:
+    # read first line
+    # assign it to string variable
+    line = fp.readline()
+    print(line)
+    
+'''
+First line
+'''
